bc52e0bad35321caf4fe7bfa0886fc03c2d56608
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2018-08-15 16:09:09 +0200

    Fix landscape or portrait orientation

--- lib/ui/intro_page.dart
+++ lib/ui/intro_page.dart
@@ -11,13 +11,16 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: bgColor,
-      padding: const EdgeInsets.all(32.0),
-      child: SafeArea(
-          child: Column(children: [
-        Expanded(child: image ?? Container(), flex: 1),
-        Expanded(child: content, flex: 1),
-      ])),
-    );
+        color: bgColor,
+        padding: const EdgeInsets.all(32.0),
+        child: OrientationBuilder(builder: (context, orientation) {
+          final isVertical = orientation == Orientation.portrait;
+
+          return SafeArea(
+              child: Column(children: [
+            Expanded(child: image ?? Container(), flex: isVertical ? 5 : 4),
+            Expanded(child: content, flex: isVertical ? 5 : 6),
+          ]));
+        }));
   }
 }
