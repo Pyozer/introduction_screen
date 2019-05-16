239398e3492e2af3bf123ca406f29571d09b48e2
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-05-16 23:39:48 +0200

    Fix formating code

--- lib/introduction_screen.dart
+++ lib/introduction_screen.dart
@@ -1,6 +1,9 @@
 library introduction_screen;
 
-export 'package:introduction_screen/src/introduction_screen.dart' show IntroductionScreen;
-export 'package:introduction_screen/src/model/page_view_model.dart' show PageViewModel;
-export 'package:introduction_screen/src/model/page_decoration.dart' show PageDecoration;
+export 'package:introduction_screen/src/introduction_screen.dart'
+    show IntroductionScreen;
+export 'package:introduction_screen/src/model/page_view_model.dart'
+    show PageViewModel;
+export 'package:introduction_screen/src/model/page_decoration.dart'
+    show PageDecoration;
 export 'package:dots_indicator/dots_indicator.dart' show DotsDecorator;
