5e19fd6cd0c5197d6cb1013261b04a98618c7d6d
maltebenjamin <46089659+maltebenjamin@users.noreply.github.com>
2021-04-26 22:08:57 +0200

    added option for ScrollController with multiple pages
    Added a option to use as many ScrollController as pages.
    That fixes the bug when tried to use one ScrollController on multiple pages.
    (conforms null safety sound)

--- lib/src/introduction_screen.dart
+++ lib/src/introduction_screen.dart
@@ -141,8 +141,8 @@ class IntroductionScreen extends StatefulWidget {
   /// A footer widget to be shown on every screen
   final Widget? globalFooter;
 
-  /// ScrollController of vertical SingleChildScrollView
-  final ScrollController? scrollController;
+  /// ScrollController of vertical SingleChildScrollView for every single page
+  final List<ScrollController?>? scrollController;
 
   /// Scroll/Axis direction of pages, can he horizontal or vertical
   ///
@@ -325,7 +325,7 @@ class IntroductionScreenState extends State<IntroductionScreen> {
                     ? widget.pages!
                         .map((p) => IntroPage(
                               page: p,
-                              scrollController: widget.scrollController,
+                              scrollController: widget.scrollController![widget.pages!.indexOf(p)],
                               isTopSafeArea: widget.isTopSafeArea,
                               isBottomSafeArea: widget.isBottomSafeArea,
                             ))
